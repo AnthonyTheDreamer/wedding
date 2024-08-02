7ba023e2b4ba09b98833e7065c8b0b0e2eaf42ef
Cong Anh Nguyen <nguyenconganh170696@gmail.com>
2024-08-02 16:03:29 +0000

    Update

--- jekyll/img/script.js
+++ jekyll/img/script.js
@@ -665,7 +665,7 @@
                 marker = new google.maps.Marker({
                 position: new google.maps.LatLng(locations[i][1], locations[i][2]),
                 map: map,
-                icon:'images/map-marker.png'
+                icon:'img/map-marker.png'
             });
 
             google.maps.event.addListener(marker, 'click', (function(marker, i) {
